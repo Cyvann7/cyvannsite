b648c9c9502ebf0edc409630b7dbfb5f6d473902
James Sheeley <James@sheeley.co.uk>
2022-12-15 23:41:41 +0000

    Complete Redesign of website - Legacy site still available within the /legacy folder.

---
+++ main.js
@@ -0,0 +1,176 @@
+
+sync("TextShake")
+
+function onLiClick(a) {
+
+	let console_line = document.createElement("p");
+	console_line.innerHTML = 'C:\\Site&gt;'+a;
+	console_line.className = "text-console";
+	document.getElementById("beforeConsole").appendChild(console_line);
+
+
+	let fakeFile = document.createElement("p");
+	fakeFile.className = "text-created";
+	let fileInner = pageSections[a];
+	//fakeFile.innerHTML = fileInner;
+	document.getElementById("beforeConsole").appendChild(fakeFile);
+	oneCharAtATime(fakeFile, fileInner);
+}
+
+function onLiHover(a) {
+	document.getElementById("consoleLine").innerHTML = 'C:\\Site&gt;'+a+'<span class="input-caret">█</span>'
+}
+
+function oneCharAtATime(el, str) {
+	console.log(str);
+	let c = str[0];
+	if (c==null) {
+		return;
+	}
+
+	let end_index = 1;
+	if (c=="<") { // Encounter a <
+		let n = "";
+		let counter=0;
+		while(counter != 2) {
+			n = str[end_index];
+			if (n === "!") { end_index = 2; break;} // so we know that it is a tag and not just a <
+
+			c+=n;
+			end_index++;
+			if (n === ">") { counter++; } // End of tag
+		}
+		
+	}
+
+	let temp = el.innerHTML;
+	el.innerHTML = temp.concat(c);
+
+	setTimeout(() => {oneCharAtATime(el, str.substring(end_index))}, 15)
+	return
+}
+
+function sync(animationNameOrNames) {
+	const animationNames = new Set(
+		Array.isArray(animationNameOrNames) ? animationNameOrNames : [animationNameOrNames]
+	);
+	const elements = new Set();
+	let animationDuration;
+	let isPaused = false;
+	let lastIterationTimestamp = 0;
+
+	const api = {
+		getElements() {
+			return elements;
+		},
+
+		free() {
+			window.removeEventListener('animationiteration', animationIteration, true);
+			window.removeEventListener('animationstart', animationStart, true);
+
+			this.start();
+			elements.clear();
+		},
+
+		start() {
+			elements.forEach((el) => {
+				if (validate(el)) {
+					if (isPaused) {
+						el.style.removeProperty('animation-play-state');
+					} else {
+						el.style.removeProperty('animation');
+					}
+				}
+			});
+			isPaused = false;
+		},
+
+		stop() {
+			isPaused = false;
+			elements.forEach((el) => {
+				if (validate(el)) {
+					el.style.setProperty('animation', 'none');
+				}
+			});
+		},
+
+		pause() {
+			isPaused = true;
+			elements.forEach((el) => {
+				if (validate(el)) {
+					el.style.setProperty('animation-play-state', 'paused');
+				}
+			});
+		}
+	};
+
+	function shouldSync(event) {
+		return animationNames.has(event.animationName);
+	}
+
+	function validate(el) {
+		const isValid = document.body.contains(el);
+		if (!isValid) {
+			elements.delete(el);
+		}
+		return isValid;
+	}
+
+	function init() {
+		setTimeout(restart, animationDuration);
+	}
+
+	function restart() {
+		api.stop();
+		setTimeout(api.start, 50);
+	}
+
+	function animationStart(event) {
+		if (shouldSync(event)) {
+			const { target: element, timeStamp } = event;
+			elements.add(element);
+
+			const diff = timeStamp - lastIterationTimestamp;
+			element.style.setProperty('animation-delay', `-${diff}ms`);
+		}
+	}
+
+
+	function animationIteration(event) {
+		if (shouldSync(event)) {
+			const { target: element, timeStamp } = event;
+			elements.add(element);
+
+			lastIterationTimestamp = timeStamp;
+
+			if (!animationDuration) {
+				animationDuration = cssToMs(window.getComputedStyle(element).animationDuration);
+				init();
+			}
+		}
+	}
+
+	window.addEventListener('animationiteration', animationIteration, true);
+	window.addEventListener('animationstart', animationStart, true);
+
+
+	return api;
+}
+
+function cssToMs(time) {
+	const num = parseFloat(time);
+	let unit = time.match(/m?s/);
+
+	if (!unit) return 0;
+
+	[unit] = unit;
+
+	switch (unit) {
+		case 's':
+			return num * 1000;
+		case 'ms':
+			return num;
+		default:
+			return 0;
+	}
+}
---
+++ pageSections.js
@@ -0,0 +1,19 @@
+pageSections = {
+	"AboutMe.txt": 
+	`I'm a student, studying Electronics, Computer Science and Maths. Currently \nworking on my GCE Advanced (A-Levels) - wish me luck!`,
+
+	"Projects.txt":
+	`<a href=\"https://github.com/Cyvann7\">Here's</a> my github. All my major projects can be found either there or on my \n<a href=\"https://cyvann7.itch.io/\">Itch.io</a> page`,
+
+	"Contacts.txt":
+	`You can email me at James@Sheeley.co.uk, or get in touch on Discord at \nCyvann7#1783`,
+	
+	"ThisSite.txt":
+	"This site uses <a href=\"https://github.com/bealearts/css-animation-sync\">this package</a> to sync animations between elements. Other than \nthat, its pure CSS, JS and HTML all the way! It's hosted via AWS Amplify, \nwith AWS Route 53 being used for the URL.",
+	
+	"Snake.txt":
+	"             ____\n            / . .\\\n            \\  ---<!\n             \\  /\n   __________/ /\n-=:___________/",
+	
+	"Legacy.txt":
+	"You can access the old version of this site by clicking <a href=\"legacy\">here</a>. It's not as \nfancy as this one, but some of the stuff on there hasn't been brought here \nyet. I'm working on it :D"
+}
