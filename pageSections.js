3f5a8c0734fe1c9f68f460a76b845bddfc3aa119
Cyvann <57076417+Cyvann7@users.noreply.github.com>
2025-01-23 20:47:05 +0000

    Update pageSections.js

--- pageSections.js
+++ pageSections.js
@@ -1,18 +1,18 @@
 pageSections = {
 	"AboutMe.txt": 
-	`I'm a student studying Computer Science at York University, currently on a gap year and looking for work to see if I prefer working to education.`,
+	`I'm a student studying Computer Science at York University, currently on a\ngap year and looking for work to see if I prefer working to education.`,
 
 	"Projects.txt":
 	`<a href=\"https://github.com/Cyvann7\">Here's</a> my github. All my major projects can be found either there or on my \n<a href=\"https://cyvann7.itch.io/\">Itch.io</a> page`,
 
 	"Contacts.txt":
-	`You can email me at James@Sheeley.co.uk, or call/text me at +44 0739 206 1822`,
+	`You can email me at <b>James@Sheeley.co.uk<\b>,\nor call/text me at +44 0739 206 1822`,
 	
 	"ThisSite.txt":
 	"This site uses <a href=\"https://github.com/bealearts/css-animation-sync\">this package</a> to sync animations between elements. Other than \nthat, its pure CSS, JS and HTML all the way! It's hosted via AWS Amplify, \nwith AWS Route 53 being used for the URL.",
 	
 	"Snake.txt":
-	"             ____\n            / . .\\\n            \\  ---<!\n             \\  /\n   __________/ /\n-=:___________/\nYou thought this would be the snake game didnt you? Got you :). But if you wanted to play snake anyway, <a href=\"https://www.jsheeley.co.uk/legacy/snake/index.html\">here.",
+	"             ____\n            / . .\\\n            \\  ---<!\n             \\  /\n   __________/ /\n-=:___________/\nYou thought this would be the snake game didnt you? Got you :).\nBut if you wanted to play snake anyway, <a href=\"https://www.jsheeley.co.uk/legacy/snake/index.html\">here.",
 	
 	"Legacy.txt":
 	"You can access the old version of this site by clicking <a href=\"legacy\">here</a>. It's not as \nfancy as this one, but some of the stuff on there hasn't been brought here \nyet."
